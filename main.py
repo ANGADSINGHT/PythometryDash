3d7069ee19622776c01a08693a9c2f75a44ee78e
ANGADSINGHT <angadpaltak@outlook.com>
2025-04-20 21:52:39 +0100

    Setting up assets

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pygame
 pygame.init()
 pygame.display.set_caption("Pythometry Dash")
 
+global screen, clock
+screen = pygame.display.set_mode(vsync=True, flags=pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+running = True
+
 class Data:
     # Colors
     WHITE: tuple[int, int, int] = (255, 255, 255)
@@ -11,34 +16,48 @@ class Data:
     # Misc
     FPS: int = 75
 
+    # Characters
+    default_avatar = pygame.image.load("./characters/default.png")
+
 class modelResponse:
     finished: bool = False
     nextFrame: int = None
-
-class ResponseTemplate:
+class Template:
     response = modelResponse
+    objects = []
 
-global screen, clock
-screen = pygame.display.set_mode(vsync=True, flags=pygame.FULLSCREEN)
-clock = pygame.time.Clock()
-running = True
+class Player:
+    def __init__(self) -> None:
+        self.avatar = Data.default_avatar
+        self.width: int = 60
+        self.height: int = 60
+        self.rotation = 0
+        self.x: int = 1920/2
+        self.y: int = 1080 - self.height
 
-class StartingScreen(ResponseTemplate):
-    def __init__(self, screen) -> None:
+    def draw(self) -> None:
+        screen.blit(self.avatar, (self.x, self.y))
+
+class StartingScreen(Template):
+    def __init__(self, screen, mainPlayer) -> None:
         self.screen = screen
+        self.objects.append(mainPlayer)
 
     def main(self) -> modelResponse:
         screen.fill(Data.WHITE)
+        self.drawObjects()
         return self.response
-
-        
-
+    
+    def drawObjects(self) -> None:
+        for object in self.objects:
+            object.draw()
             
 
-frames: dict[int, ResponseTemplate] = {0: StartingScreen}
+frames: dict[int, Template] = {0: StartingScreen}
 frameCount: int = 0
-loadedFrames: list[ResponseTemplate] = [frames[0](screen)]
-currentFrame: ResponseTemplate = loadedFrames[0]
+mainPlayer: Player = Player()
+loadedFrames: list[Template] = [frames[0](screen, mainPlayer)]
+currentFrame: Template = loadedFrames[0]
 
 if __name__ == "__main__":
     while running:
